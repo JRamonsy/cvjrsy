1b7d91d10adc78682575747ec0f6c4410a8dbd1e
JRamonsy <ramon.salaspi1.1@gmail.com>
2024-08-04 17:14:25 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
-    },
-  },
-})
+  plugins: [
+    react(),
+    {
+      name: 'tailwindcss',
+      config: tailwindcss('./tailwind.config.js')
+    }
+  ]
+});
